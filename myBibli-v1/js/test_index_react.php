bd1566dae002475171ed6f9858c5d6ddf5bdc687
Quentins <HE201860@students.ephec.be>
2021-03-24 12:36:43 +0100

    intégration API

--- myBibli-v1/js/test_index_react.php
+++ myBibli-v1/js/test_index_react.php
@@ -25,6 +25,31 @@
 		function LinkElement() {
     return <div className='connexion'><a href={"pageConnexion.php"}>Se connecter</a></div>
 		}
+		function rechercherLivre(){
+	    let champNom = document.getElementById("barreDeRecherche").value;
+			console.log(champNom);
+	    let xhr = new XMLHttpRequest();
+	    xhr.onload = recupererDonnees;
+	    xhr.open("get", `https://www.googleapis.com/books/v1/volumes?q=${champNom}`, true);
+	    xhr.send();
+		}
+
+		function recupererDonnees() {
+		    let traiter = JSON.parse(this.responseText);
+				let count = traiter.items.length
+				document.getElementById("counter").innerHTML = "<p>Votre recherche à rapporté " + count + " résultats</p>";
+				document.getElementById("containerLivres").innerHTML = "";
+				for(let i = 0; i < Number(count); i++){
+				let imageLivre = traiter["items"][i].volumeInfo.imageLinks.thumbnail
+				document.getElementById("containerLivres").innerHTML += "<p>" + traiter["items"][i].volumeInfo.title + "</p>";
+				document.getElementById("containerLivres").innerHTML += `<img src='${imageLivre}'>`;
+				document.getElementById("containerLivres").innerHTML += "<p>" + traiter["items"][i].volumeInfo.authors + "</p>";
+
+
+
+			}
+
+		}
 
 
 		ReactDOM.render(<div id="tete"><Header/><LinkElement/></div>,
@@ -35,18 +60,22 @@
 
 		<form id="rechercheLivre">
             <label for="barreDeRecherche"></label><input type="text" id="barreDeRecherche" value="Barre de recherche">
-			<input type="button" id="recherchePublique" value="Publique">
+			<input type="button" id="recherchePublique" value="Chercher" onclick="rechercherLivre()">
 		</form>
+		<div id="counter"></div>
 		<div id="containerLivres">
 
 				<?php
 				echo $donneesLivres["idLivre"];
 					while($donneesLivres = $reponse->fetch())
 					{
-						echo "<section id='{$donneesLivres["idLivre"]}' class='box_livre'><p>" . $donneesLivres["Auteur"] . "<br><br>" . $donneesLivres["titre"] . "</p>" . "<img src='" . $donneesLivres["premiereDeCouverture"] . "'><br><br> Synopsis :<br>" . $donneesLivres["synopsis"] . "</section>";
+						echo "<section id='{$donneesLivres["idLivre"]}' class='box_livre' onclick='location.href' == '''pageLivre.php'''><p>" . $donneesLivres["Auteur"] . "<br><br>" . $donneesLivres["titre"] . "</p>" . "<img src='" . $donneesLivres["premiereDeCouverture"] . "'><br><br> Synopsis :<br>" . $donneesLivres["synopsis"] . "</section>";
 					}
 					$reponse->closeCursor();
 				?>
+				<script>
+
+				</script>
 		</div>
 	</body>
 
