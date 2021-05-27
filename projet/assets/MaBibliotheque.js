adaad2b61760ae54f5871eea9a663ed23e3dc9fe
sebastienvolont <he201833@students.ephec.be>
2021-05-27 12:00:12 +0200

    [Frontend] Ajout des filtres de la bibliotheque et changement statut livre

--- projet/assets/MaBibliotheque.js
+++ projet/assets/MaBibliotheque.js
@@ -1,4 +1,3 @@
-import './styles/app.css';
 import './bootstrap';
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -11,16 +10,18 @@ class MaBibliotheque extends React.Component {
         this.state = {
             livres: [],
             isLoaded : false,
-            tri: ''
+            tri: '',
+            filtreGenre : '',
+            filtreStatut: '',
+            caseChoisie : '',
         }
+        this.changementStatutLivre = this.changementStatutLivre.bind(this)
     }
 
     refresh () {
         fetch("http://localhost:8000/api/livres", {method: 'GET'})
             .then(livresItems => livresItems.json())
             .then((response) => {
-                // console.log(result["hydra:member"][0]["titre"]);
-                console.log(response["hydra:member"][0].titre)
                 this.setState({
                         isLoaded : true,
                         livres : response["hydra:member"]
@@ -30,20 +31,66 @@ class MaBibliotheque extends React.Component {
             
     }
 
+    reinitialiser () {
+        this.setState({ tri: ""});
+        this.setState({ filtreGenre: ""});
+        this.setState({ filtreStatut : ""});
+    }
+
     handleSort = (e) => {
-        console.log(e.target.value);
         this.setState({ tri: e.target.value})
     }
 
+    
+    handleFilter = (e) => {
+        this.setState({ filtreGenre: e.target.value})
+    }
+
+    handleFilterStatut = (e) => {
+        this.setState({ filtreStatut: e.target.value})
+    }
+
+    handleCheckBox = (e) => {
+        this.setState({ caseChoisie: e.target.value})
+    }
+
+
+    changementStatutLivre (id) {
+        let statut={
+            "statut" : this.state.caseChoisie
+        }
+
+    
+        fetch("http://localhost:8000/api/livres/" + id, {method: 'PUT',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify(statut)
+    })
+        .then(response => response.json())
+        .then(this.refresh.bind(this))
+    }
+
+
     render() {
-        const {livres} = this.state;
+        let {livres} = this.state;
+        
+        if (this.state.filtreGenre != ""){
+        livres = this.state.livres.filter(livre => livre.genre == this.state.filtreGenre);
+        }
+
+        if (this.state.filtreStatut != ""){
+            livres = this.state.livres.filter(livre => livre.statut == this.state.filtreStatut);
+            }
+
+
 
         const triLivres = this.state.livres.sort((a, b) => {
-            if(this.state.tri === 'Sort croissant') {
+            if(this.state.tri === 'Tri croissant') {
                 if (a.titre > b.titre) { return 1}
                 if (a.titre < b.titre) { return -1}
             }
-            if(this.state.tri === 'Sort decroissant') {
+            if(this.state.tri === 'Tri decroissant') {
                 if (a.titre > b.titre) { return -1}
                 if (a.titre < b.titre) { return 1}
             }
@@ -59,16 +106,33 @@ class MaBibliotheque extends React.Component {
             
         })
 
+        const livresTrouvees = () => {
+            if (livres.length < 1) {
+                return <div className="alert alert-danger" role="alert">
+                        Pas de résultat lié à cette recherche
+                </div>
+            }
+        }
+
+
         window.onload = this.refresh.bind(this)
-
+                                            
         return (
-
+            
             <div className={"container-fluid"}>
-                <FiltreLivre handleSort={this.handleSort} />
+                
+                
+
+                    
+                <FiltreLivre handleSort={this.handleSort} handleFilter={this.handleFilter} handleFilterStatut={this.handleFilterStatut} handleCheckBox={this.handleCheckBox} reinitialiser={this.reinitialiser.bind(this)}/>
+                
+                
                     {livres.map((livre, i) => (
-                        <Bibliotheque key={i} couverture={livre.couverture} titre={livre.titre} synopsis={livre.synopsis} parution={livre.parution} />    
+                        <Bibliotheque handleCheckBox={this.handleCheckBox} changementStatutLivre={this.changementStatutLivre} key={i} couverture={livre.couverture} titre={livre.titre} synopsis={livre.synopsis} parution={livre.parution} statut={livre.statut} idLivre={livre.idLivre} />    
                     ))}
-            </div>
+                    {livresTrouvees()}
+                </div>
+                
         )
 
     }
@@ -78,54 +142,97 @@ class MaBibliotheque extends React.Component {
 function FiltreLivre(props){
     return (
         <div className={"container divFiltres"}>
-            <form method="post" action="">
-                <select className="form-select form-select-lg" aria-label="Default select example" onChange={props.handleSort}>
+                <select className="form-select form-select-lg" aria-label="Default select example" onChange={props.handleSort} id="TriCroissantDecroissant">
                     <option defaultValue>Trier par ordre</option>
-                    <option value="Sort croissant">Alphabétique croissant</option>
-                    <option value="Sort decroissant">Alphabétique décroissant</option>
+                    <option value="Tri croissant">Alphabétique croissant</option>
+                    <option value="Tri decroissant">Alphabétique décroissant</option>
                     <option value="Parution croissant">Parution croissant</option>
                     <option value="Parution decroissant">Parution décroissant</option>
                 </select>
-                <select className="form-select form-select-lg" aria-label="Default select example">
-                    <option defaultValue>Genre littéraire</option>
-                    <option value="1">Fiction</option>
-                    <option value="2">Drama</option>
-                    <option value="3">Biography & Autobiography</option>
+                <select className="form-select form-select-lg" aria-label="Default select example" onChange={props.handleFilter} id="triGenre">
+                    <option defaultValue value="">Genre littéraire</option>
+                    <option value="Fiction">Fiction</option>
+                    <option value="Drama">Drame</option>
+                    <option value="Biography & Autobiography">Biographie & Autobiographie</option>
                 </select>
-                <select className="form-select form-select-lg prima" aria-label="Default select example">
-                    <option defaultValue>Langue</option>
-                    <option value="en">en</option>
-                    <option value="fr">fr</option>
+                <select className="form-select form-select-lg prima" aria-label="Default select example" onChange={props.handleFilterStatut} id="triStatut">
+                    <option defaultValue value="">Statut</option>
+                    <option value="Non lu">Non lu </option>
+                    <option value="En cours">En cours</option>
+                    <option value="Terminé">Terminé</option>
                 </select>
-                <select className="form-select form-select-lg" aria-label="Default select example">
-                    <option defaultValue>Nombre de pages</option>
-                    <option value="1"> Moins de 50 </option>
-                    <option value="2"> Plus de 100 </option>
+                
+            <button className="btn btn-info" onClick={props.reinitialiser}> Réinitialiser les filtres</button>
+            
 
-                </select>
-            </form>
         </div>
     )
 }
 
 function Bibliotheque (props){
         return (
-            <div className={"container divBiblio"}>
-        <div className="row" style={{border: "2px solid black", backgroundColor : "grey"}}>
+            <div className={"container-fluid divBiblio"}>
+            <div className="col md-6 lg-6">
+                <div className="jumbotron">
             <div className="col-xs-10 col-sm-2">
-                <img src={props.couverture} className="card-img-top" alt="..." />
+                <img src={props.couverture} className="card-img-top" alt="couverture livre" />
             </div>
             <div className="col-xs-10 col-sm-8">
-                <h4 className="card-title">{props.titre} | {props.parution}</h4>
-                <p className="card-text" style={{color : "white"}}>
+                <h4 className="card-title">{props.titre} | {props.parution} | {props.statut}</h4>
+                <p className="card-text" style={{color : "black"}}>
                     {props.synopsis}
                 </p>
+                <a href={"http://localhost:8000/afficherLivre/" + props.idLivre}><button className="btn btn-primary"> Afficher plus d'informations </button></a>
             </div>
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="Non lu" id={"livreNonLu" + props.idLivre} name="statutLivre" onChange={props.handleCheckBox} onClick={() => choixCheckbox("livreFini" + props.idLivre, "livreEnCours" + props.idLivre, "livreNonLu" + props.idLivre)}/>
+                <label className="form-check-label" htmlFor={"livreNonLu" + props.idLivre}>
+                    Non lu
+                </label> <br></br>
+
+                <input className="form-check-input" type="checkbox" value="En cours" id={"livreEnCours" + props.idLivre} name="statutLivre" onChange={props.handleCheckBox} onClick={() => choixCheckbox("livreFini" + props.idLivre, "livreEnCours" + props.idLivre, "livreNonLu" + props.idLivre)}/> 
+                <label className="form-check-label" htmlFor={"livreEnCours" + props.idLivre}>
+                    En cours
+                </label> <br></br>
+
+                <input className="form-check-input" type="checkbox" value="Terminé" id={"livreFini" + props.idLivre} name="statutLivre" onChange={props.handleCheckBox} onClick={() => choixCheckbox("livreFini" + props.idLivre, "livreEnCours" + props.idLivre, "livreNonLu" + props.idLivre)}/> 
+                <label className="form-check-label" htmlFor={"livreFini" + props.idLivre}>
+                    Terminé
+                </label> <br></br>
+
+                <button className={"btn btn-primary"} onClick={props.changementStatutLivre.bind(props,props.idLivre)}>Changer le statut</button>
+                </div>
+                </div>
+                </div>
         </div>
-        </div>
+
         )
 }
 
+
+function choixCheckbox (documentId1, documentId2, documentId3) {
+    let isSelectedCheckBox1 = document.getElementById(documentId1);
+    let isSelectedCheckBox2 = document.getElementById(documentId2);
+    let isSelectedCheckBox3 = document.getElementById(documentId3);
+
+
+    if (isSelectedCheckBox1.checked == true) {
+        isSelectedCheckBox2.checked = false
+        isSelectedCheckBox3.checked = false
+    }
+
+    else if (isSelectedCheckBox2.checked == true) {
+        isSelectedCheckBox1.checked = false
+        isSelectedCheckBox3.checked = false
+    }
+
+    else if (isSelectedCheckBox3.checked == true) {
+        isSelectedCheckBox1.checked = false
+        isSelectedCheckBox2.checked = false
+    }
+
+
+}
 
 ReactDOM.render(<MaBibliotheque />, document.getElementById("sectionBibliotheque"))
 
